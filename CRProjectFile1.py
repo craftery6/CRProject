1d2c6933f7e0a75e86566c1936f67e0dc652dcba
craftery <craftery@mayococo.ie>
2021-04-23 16:36:10 +0100

    "Second Commit"

--- CRProjectFile1.py
+++ CRProjectFile1.py
@@ -7,3 +7,23 @@ print(dataset.head())
 
 ## print missing values
 print(dataset.isnull().sum())
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+## define dataframe df
+
+df = pd.read_csv("CrimeUSA.csv")
+print(df.head())
+print(df.tail())
+
+##show the amount of rows
+len(df)
+
+##sort data by 'violent_crime'
+df.sort_values(by=['violent_crime'])
+
+##selecting a subset of the data
+df['theft']
+
