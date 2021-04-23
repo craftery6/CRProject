6abbbb90a5f3dfc99e536d08b291783c0a8c11bd
craftery <craftery@mayococo.ie>
2021-04-23 18:01:43 +0100

    "Third Commit"

--- CRProjectFile1.py
+++ CRProjectFile1.py
@@ -19,11 +19,36 @@ print(df.head())
 print(df.tail())
 
 ##show the amount of rows
-len(df)
+print(len(df))
 
 ##sort data by 'violent_crime'
 df.sort_values(by=['violent_crime'])
+print(df)
 
 ##selecting a subset of the data
-df['theft']
+print(df['theft'])
 
+##selecting multiple columns
+print(df[['theft','rape']])
+
+##show the details of the first column, 'location'
+print(df.loc[0])
+
+##Create a new list called 'crimes' and print the contents
+crimes = ["theft", "rape", "burglary", "robbery", "violent_crime", "murder_ms", "agg_assault", "prop_crimes"]
+for item in crimes:
+    print(item)
+
+##Find the index value of 'robbery' in the list
+item2 = "robbery"
+index = crimes.index(item2)
+print(index)
+
+##slicing a list. Select the first 5 rows of the dataset
+print(df[:5])
+
+##select the last row of the list
+print(df[-1:])
+
+##selecting a subset of rows and columns within the dataframe 'df' to show the violent crime, murder/ manslaughter stats and rape stats of 3 differeyt locations.
+print(df.iloc[1:4, 0:4])
